4efb1685e5d29ff1f95ad05e26486b8c708fe73b
Marco Gonçalves <marco99goncalves@gmail.com>
2022-08-21 13:41:34 +0100

    Episode 9

--- TheChernoOpenGL/Application.cpp
+++ TheChernoOpenGL/Application.cpp
@@ -109,22 +109,32 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << "\n";
 
-    float vertices[6] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.0f,  0.5f
+    float vertices[] = {
+         0.0f,  0.0f,
+         0.5f,  0.0f,
+         0.5f,  0.5f,
+         0.0f,  0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 3, 2
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), vertices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
     ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
-    std::cout << source.VertexSource << " " << source.FragmentSource;
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
@@ -134,7 +144,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
